6f58402276f9c231ca980a9b5246ed2d51127b0c
Kasparri <Kasperlsorensen@gmail.com>
2017-01-02 10:54:46 +0100

    Initial Commits

--- Dialogue.fsx
+++ Dialogue.fsx
@@ -1,6 +1,7 @@
 // Applied Functional Programming 02257
-// Project 1 - Nim Game - Model
+// Project 1 - Nim Game - Dialogue
 
 // Mads Ejdesgaard Maibohm s144479
 // Kasper Lederballe Sørensen s144453
+
 
