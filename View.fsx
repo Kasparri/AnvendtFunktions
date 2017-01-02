d5dd08095232605b8e2e929d30528e9a70389eac
Kasparri <Kasperlsorensen@gmail.com>
2017-01-02 12:05:09 +0100

    Playing around with view

--- View.fsx
+++ View.fsx
@@ -5,3 +5,43 @@
 // Kasper Lederballe Sørensen s144453
 
 
+// Prelude
+open System 
+open System.Net 
+open System.Threading 
+open System.Windows.Forms 
+open System.Drawing 
+
+let window = new Form(Text="Nim Game", Size=Size(700,700))
+
+let urlBox = new TextBox(Location=Point(10,10),Size=Size(400,25))
+let loadButton = new Button(Location=Point(415,10),MinimumSize=Size(50,25),
+                 MaximumSize=Size(50,25),Text="Load Game")
+
+
+let takeButton = new Button(Location=Point(575,10),MinimumSize=Size(100,75),
+                 MaximumSize=Size(100,75),Text="Take!")
+
+let clearButton = new Button(Location=Point(575,650),MinimumSize=Size(100,25),
+                  MaximumSize=Size(100,25),Text="Clear Game")
+
+
+
+
+// Initialization
+
+
+
+window.Controls.Add urlBox
+window.Controls.Add loadButton
+
+window.Controls.Add takeButton
+
+window.Controls.Add clearButton
+
+// Start
+
+//Async.StartImmediate (empty())
+
+Application.Run(window) (* Mac *)
+//window.Show() (* Windows *)
