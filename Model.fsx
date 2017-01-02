905d7cf8e42fd340a105c25f9ec43b425023c658
Stolpe plix <mads_ccc@hotmail.com>
2017-01-02 12:13:21 +0100

    start of model

--- Model.fsx
+++ Model.fsx
@@ -3,4 +3,44 @@
 
 // Mads Ejdesgaard Maibohm s144479
 // Kasper Lederballe Sørensen s144453
+type AsyncEventQueue<'T>() = 
+    let mutable cont = None 
+    let queue = System.Collections.Generic.Queue<'T>()
+    let tryTrigger() = 
+        match queue.Count, cont with 
+        | _, None -> ()
+        | 0, _ -> ()
+        | _, Some d -> 
+            cont <- None
+            d (queue.Dequeue())
 
+    let tryListen(d) = 
+        if cont.IsSome then invalidOp "multicast not allowed"
+        cont <- Some d
+        tryTrigger()
+
+    member x.Post msg = queue.Enqueue msg; tryTrigger()
+    member x.Receive() = 
+        Async.FromContinuations (fun (cont,econt,ccont) -> 
+            tryListen cont)
+
+let sticks = [5;4;3]
+
+let ev = AsyncEventQueue()
+
+let rec ready() = async {
+         let! msg = ev.Receive()
+         match msg with
+            | clear -> return! ready()
+            | take  -> return! failwith("ready: No game started yet")
+            | _     -> failwith ("ready: updating message")
+
+         };;
+
+and loading =
+
+and player =
+
+and AI =
+
+and finished =
