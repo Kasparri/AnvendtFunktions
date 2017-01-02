9f17a5f6c936d499d1e57a4246dacf022c3e76eb
Kasparri <Kasperlsorensen@gmail.com>
2017-01-02 13:41:51 +0100

    one file

--- Model.fsx
+++ Model.fsx
@@ -131,3 +131,60 @@ let buildList html = buildList' (explode html)
 
 buildList "1";;
 
+
+
+
+let window = new Form(Text="Nim Game", Size=Size(700,700))
+
+let urlBox = new TextBox(Location=Point(10,10),Size=Size(400,25))
+
+let ansBox =
+  new TextBox(Location=Point(150,150),Size=Size(200,25))
+
+
+
+let loadButton = new Button(Location=Point(415,10),MinimumSize=Size(50,25),
+                  MaximumSize=Size(50,25),Text="Load Game")
+
+let cancelButton = new Button(Location=Point(470,10),MinimumSize=Size(50,25),
+                    MaximumSize=Size(50,25),Text="Cancel load")
+
+
+let takeButton = new Button(Location=Point(575,10),MinimumSize=Size(100,75),
+                  MaximumSize=Size(100,75),Text="Take!")
+
+let clearButton = new Button(Location=Point(575,650),MinimumSize=Size(100,25),
+                   MaximumSize=Size(100,25),Text="Clear Game")
+
+                   
+
+(*
+let disable bs = 
+    for b in [startButton;clearButton;cancelButton] do 
+        b.Enabled  <- true
+    for (b:Button) in bs do 
+        b.Enabled  <- false
+        *)
+
+// Initialization
+
+
+// view
+
+window.Controls.Add urlBox
+window.Controls.Add loadButton
+window.Controls.Add cancelButton
+
+window.Controls.Add takeButton
+
+window.Controls.Add clearButton
+
+window.Controls.Add ansBox
+
+// Start
+
+//Async.StartImmediate (empty())
+
+//Application.Run(window) (* Mac *)
+window.Show() (* Windows *)
+
