01c4f45da78d4716e23d699a4e3122cd0fbf26c8
Kasparri <Kasperlsorensen@gmail.com>
2017-01-05 11:01:37 +0100

    Changes

--- ModelDialogue.fsx
+++ ModelDialogue.fsx
@@ -1,8 +1,10 @@
 // Applied Functional Programming 02257
-// Project 1 - Nim Game - Model
+// Project 1 - Nim Game - Model & Dialogue
 
 // Mads Ejdesgaard Maibohm s144479
 // Kasper Lederballe Sørensen s144453
+
+// Prelude
 
 open System 
 open System.Net 
@@ -17,7 +19,19 @@ open EventQueue
 #r "View.dll"
 open View
 
-// Model
+
+(* Model *)
+
+type Message =
+  | Take of (int*int) | Clear 
+  | Cancel | Web of string 
+  | Error | Cancelled 
+  | Load of (int*int*int) | Fetch
+  | Generate | Create of (int*int*int)
+
+let mutable sticks = Array.empty
+let mutable taunted = false
+let ev:AsyncEventQueue<Message> = AsyncEventQueue()
 
 let checkBox s = 
     let mutable result = 0
@@ -29,26 +43,55 @@ let checkBoxMax s max =
     if Int32.TryParse(s, &result) && result > 0 && result <= max then true
     else false
 
+let victorycheck A = Array.forall (fun i -> i = 0) A
+
+let makeArray (html:string) = Array.map int ((html.Trim()).Split (' ', '\n'))
+
+let consURL n min max = sprintf "https://www.random.org/integers/?num=%d&min=%d&max=%d&col=1&base=10&format=plain&rnd=new" n min max
+  
+let getM a = Array.fold (fun s v -> s^^^v ) 0 a
+
+let movePred ak m = (ak ^^^ m) < ak
+
+let difficultyCheck value = let rand = System.Random()
+                            rand.Next (1,100) >= value
+
+let randAmountHeap() = let rand = System.Random()
+                       let index = rand.Next(1, ((sticks.Length)-1))
+                       if sticks.[index] > 0 then (rand.Next (1,sticks.[index]),index)
+                       else (0,0)
+
+
+let generateGame h min max = 
+    let mutable game = List.empty
+    let rand = System.Random()
+    if min >= 1 && max > min && h > 0 && h <= 9 then
+        for i = 1 to h do 
+            game <- (rand.Next(min,max))::game
+    else
+        for i = 1 to (rand.Next(1,9)) do
+            game <- (rand.Next(1,20))::game
+    List.toArray game
+
+
+(* Dialogue *)
+
 let checkFetchBoxes () =
     if (checkBoxMax heapBox.Text 9) && (checkBox minBox.Text) && (checkBox maxBox.Text)
      then ((int (minBox.Text)) < (int (maxBox.Text)))
      else false
 
-type Message =
-  | Take of (int*int) | Clear | Cancel | Web of string | Error | Cancelled | Load of (int*int*int) | Fetch
+let checkGenerateBoxes () =
+    if (checkBoxMax gheapBox.Text 9) && (checkBox gminBox.Text) && (checkBox gmaxBox.Text)
+     then ((int (gminBox.Text)) < (int (gmaxBox.Text)))
+     else false
 
-let mutable sticks = Array.empty
 let mutable heapButtons:Button list = List.empty
-let mutable taunted = false
-let ev:AsyncEventQueue<Message> = AsyncEventQueue()
 
 let resetVariables () = sticks <- Array.empty
                         heapButtons <- List.empty
                         taunted <- false
 
-let victorycheck A = Array.forall (fun i -> i = 0) A
-
-let makeArray (html:string) = Array.map int ((html.Trim()).Split (' ', '\n'))
 
 let setButtonTexts() = for i = 0 to sticks.Length-1 do
                         heapButtons.[i].Text <- string sticks.[i]
@@ -58,22 +101,9 @@ let takeAction n i (a:(int [])) =
     else a.[i] <- (a.[i]-n)
          setButtonTexts()
     
-let consURL n min max = sprintf "https://www.random.org/integers/?num=%d&min=%d&max=%d&col=1&base=10&format=plain&rnd=new" n min max
-  
-let getM a = Array.fold (fun s v -> s^^^v ) 0 a
-
-let movePred ak m = (ak ^^^ m) < ak
-
-let difficultyCheck() = let rand = System.Random()
-                        rand.Next (1,100) >= difficultySlider.Value
-
-let randAmountHeap() = let rand = System.Random()
-                       let index = rand.Next(1, ((sticks.Length)-1))
-                       if sticks.[index] > 0 then (rand.Next (1,sticks.[index]),index)
-                       else (0,0)
 
 let makeZeroMove array m =
-    if difficultyCheck() then 
+    if difficultyCheck difficultySlider.Value then 
         let id = Array.findIndex (fun ak -> movePred ak m ) array
         let diff = sticks.[id] - (sticks.[id] ^^^ m)
         if (not taunted) then ansBox.Text <- sprintf "you will lose ( ͠° ͟ʖ ͡°)"
@@ -105,22 +135,37 @@ let createHeapButtons() =
                   heapButtons <- List.rev heapButtons
                   setButtonTexts()
 
+
 let rec ready() = 
          async {
-         disable [cancelButton;fetchOKButton;cancelFetchButton]
+         disable [cancelButton;fetchOKButton;cancelFetchButton;
+                  cancelGenerateButton;generateOKButton]
          let! msg = ev.Receive()
          match msg with
-            | Clear -> return! ready()
-            | Fetch -> return! fetching()
-            | _     -> failwith ("ready: unexpected message")
+            | Clear    -> return! ready()
+            | Fetch    -> return! fetching()
+            | Generate -> return! generating()
+            | _        -> failwith ("ready: unexpected message")
          }
 and fetching() = 
           async {
-          disable [clearButton;fetchButton;cancelButton]
+          disable [clearButton;fetchButton;cancelButton;
+                   cancelGenerateButton;generateOKButton;generateButton]
           let! msg = ev.Receive()
           match msg with
             | Clear  -> return! fetching()
             | Load (n,min,max)  -> return! loading(consURL n min max)
+            | Cancel -> return! ready()
+            | _      -> failwith ("loading:unexpected message")
+         }
+and generating() = 
+          async {
+          disable [clearButton;fetchButton;cancelButton;
+                   fetchOKButton;cancelFetchButton;generateButton]
+          let! msg = ev.Receive()
+          match msg with
+            | Clear  -> return! generating()
+            | Create (h,min,max)  -> return! creating(h,min,max)
             | Cancel -> return! ready()
             | _      -> failwith ("loading:unexpected message")
          }
@@ -137,13 +182,14 @@ and loading(url) =
               (fun _ -> ev.Post Cancelled),
               ts.Token)
         
-         disable [clearButton;fetchButton;fetchOKButton;cancelFetchButton]
+         disable [clearButton;fetchButton;fetchOKButton;cancelFetchButton;
+                  cancelGenerateButton;generateOKButton;generateButton]
          let! msg = ev.Receive()
          match msg with
             | Web html -> sticks <- makeArray html
                           createHeapButtons()
                           return! player()
-            | Error -> sticks <- [| 1;2;3 |]
+            | Error -> sticks <- generateGame (int heapBox.Text) (int minBox.Text) (int maxBox.Text)
                        createHeapButtons()
                        return! player()
             | Cancel -> ts.Cancel()
@@ -151,10 +197,19 @@ and loading(url) =
             | _     -> failwith ("loading:unexpected message")
 
          }
+and creating(h,min,max) =
+  async {
+           disable [fetchButton;fetchOKButton;cancelFetchButton;clearButton;
+                    cancelGenerateButton;generateOKButton;cancelButton;generateButton]
+           sticks <- generateGame h min max
+           createHeapButtons()
+           return! player()
+       }
 and cancelling() = 
   async {
          
-         disable [clearButton; cancelButton;cancelFetchButton]
+         disable [clearButton; cancelButton;cancelFetchButton;
+                  cancelGenerateButton;generateOKButton;generateButton]
          let! msg = ev.Receive()
          match msg with
          | Cancelled | Error | Web  _ ->
@@ -162,10 +217,11 @@ and cancelling() =
          | _    ->  failwith("cancelling: unexpected message")
          }
 and player() = 
-    async {
+  async {
     if victorycheck sticks then return! finished("AI won") 
     else
-    disable [fetchButton;fetchOKButton;cancelButton;cancelFetchButton]
+    disable [fetchButton;fetchOKButton;cancelButton;cancelFetchButton;
+             cancelGenerateButton;generateOKButton;generateButton]
 
     let! msg = ev.Receive()
     match msg with
@@ -183,7 +239,8 @@ and AI() =
     async {
     if victorycheck sticks
     then return! finished("Player won")
-    else disable [fetchButton;cancelButton;fetchOKButton;clearButton]
+    else disable [fetchButton;fetchOKButton;cancelFetchButton;clearButton;
+                  cancelGenerateButton;generateOKButton;cancelButton;generateButton]
          aiMove()
          do! Async.Sleep 1000
          for i=0 to sticks.Length-1 do
@@ -193,7 +250,8 @@ and AI() =
 and finished(s) =
     async {
     ansBox.Text <- s
-    disable [cancelButton;fetchButton;fetchOKButton;cancelFetchButton]
+    disable [cancelButton;fetchButton;fetchOKButton;cancelFetchButton;
+             cancelGenerateButton;generateOKButton;generateButton]
     let! msg = ev.Receive()
     match msg with
         | Clear -> resetVariables()
@@ -202,8 +260,17 @@ and finished(s) =
     }
 
 // Initialization
+(* Main window *)
 
-
+fetchButton.Click.Add ( fun _ -> ev.Post Fetch
+                                 fetchWindow.Show() )
+generateButton.Click.Add ( fun _ -> ev.Post Generate
+                                    generateWindow.Show() )  
+cancelButton.Click.Add ( fun _ -> ev.Post Cancel )
+clearButton.Click.Add ( fun _ -> ansBox.Text <- ""
+                                 for i = 0 to (sticks.Length) - 1 do
+                                   window.Controls.Remove heapButtons.[i]
+                                 ev.Post Clear )
 
 slider.Scroll.Add ( fun _ -> sliderBox.Text <- slider.Value.ToString() )
 sliderBox.TextChanged.Add ( fun _ -> if checkBoxMax (sliderBox.Text) (slider.Maximum) 
@@ -211,31 +278,33 @@ sliderBox.TextChanged.Add ( fun _ -> if checkBoxMax (sliderBox.Text) (slider.Max
                                      else slider.Value <- slider.Value )
 
 
-fetchButton.Click.Add ( fun _ -> ev.Post Fetch
-                                 fetchWindow.Show() )
+(* Fetch window *)
 
-cancelButton.Click.Add ( fun _ -> ev.Post Cancel )
-
-clearButton.Click.Add ( fun _ -> ansBox.Text <- ""
-                                 for i = 0 to (sticks.Length) - 1 do
-                                   window.Controls.Remove heapButtons.[i]
-                                 ev.Post Clear )
-
-
-difficultySlider.Scroll.Add ( fun _ -> difficultySliderBox.Text <- toDifficulty difficultySlider.Value)
-
-                              
 fetchOKButton.Click.Add ( fun _ -> if checkFetchBoxes() 
                                    then ( ev.Post (Load( (int heapBox.Text) , (int minBox.Text) , (int maxBox.Text) ) ) ) 
                                         ( slider.Maximum <- (int maxBox.Text) )
                                         ( fetchWindow.Hide() )
                                    else ev.Post Clear )
-
 cancelFetchButton.Click.Add ( fun _ -> ev.Post Cancel
                                        fetchWindow.Hide() )
 
-// Start
+difficultySlider.Scroll.Add ( fun _ -> difficultySliderBox.Text <- toDifficulty difficultySlider.Value)
+
+(* Generate window *)
+
+generateOKButton.Click.Add ( fun _ -> if checkGenerateBoxes() 
+                                      then ( ev.Post (Create( (int gheapBox.Text) , (int gminBox.Text) , (int gmaxBox.Text) ) ) ) 
+                                           ( slider.Maximum <- (int gmaxBox.Text) )
+                                           ( generateWindow.Hide() )
+                                      else ev.Post Clear )
+cancelGenerateButton.Click.Add ( fun _ -> ev.Post Cancel
+                                          generateWindow.Hide() )
+
+gdifficultySlider.Scroll.Add ( fun _ -> gdifficultySliderBox.Text <- toDifficulty gdifficultySlider.Value)
+
+
+// Start the program
 Async.StartImmediate (ready())
 
-//Application.Run(window) (* Mac *)
-window.Show() (* Windows *)
+Application.Run(window) (* Mac *)
+//window.Show() (* Windows *)
